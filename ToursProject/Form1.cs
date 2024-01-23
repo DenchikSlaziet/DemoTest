3f576dca259225657c2726596bccf9b44c414f2b
DenchikSlaziet <kochetkovdenic1@gmail.com>
2024-01-23 21:34:50 +0300

    Сделал вывод (предварительно, плохой код)

--- ToursProject/Form1.cs
+++ ToursProject/Form1.cs
@@ -1,12 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using ToursProject.Context.Models;
+using ToursProject.UserControls;
 
 namespace ToursProject
 {
@@ -15,6 +12,39 @@ namespace ToursProject
         public Form1()
         {
             InitializeComponent();
+            var boxes = new List<UserView>();
+            var tours = new List<Tour>();
+
+            for (int i = 0; i < 12; i++)
+            {
+                var tour = new Tour
+                {
+                    Id = i,
+                    Title = string.Join("",Guid.NewGuid().ToString().Take(7)),
+                    IsActual = true,
+                    Price = new Random().Next(100,100000),
+                    TicketCount = new Random().Next(1, 100)
+                };
+                tours.Add(tour);
+            }
+
+            var y = 10;
+            var n = 0;
+            for (int i = 0; i < tours.Count; i++, n++)
+            {
+                if (i % 3 == 0 && i != 0)
+                {
+                    n = 0;
+                    y += 100;
+                }
+
+                var box = new UserView(tours[i], 250 * n, y, i);
+                boxes.Add(box);
+                Controls.Add(box.Title);
+                Controls.Add(box.Price);
+                Controls.Add(box.CountTicket);
+                Controls.Add(box.Actual);
+            }
         }
     }
 }
---
+++ ToursProject/UserControls/UserView.cs
@@ -0,0 +1,54 @@
+﻿using System.Windows.Forms;
+using ToursProject.Context.Models;
+
+namespace ToursProject.UserControls
+{
+    public class UserView
+    {
+        private Tour tour { get; set; }
+        public Label Title { get; set; } = new Label();
+        public Label Price { get; set; } = new Label();
+        public Label Actual { get; set; } = new Label();
+        public Label CountTicket { get; set; } = new Label();
+
+        public UserView(Tour tour, int x, int y, int number)
+        {
+            this.tour = tour;
+
+            Title.AutoSize = true;
+            Title.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            Title.Location = new System.Drawing.Point(x+50, y);
+            Title.Name = $"Title_{number}";
+            Title.TabIndex = number;
+            Title.Text = tour.Title;
+
+            Price.AutoSize = true;
+            Price.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            Price.Location = new System.Drawing.Point(x+50, y+30);
+            Price.Name = $"Price_{number}";
+            Price.TabIndex = number;
+            Price.Text = $"{tour.Price} руб.";
+
+            Actual.AutoSize = true;
+            Actual.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            Actual.Location = new System.Drawing.Point(x, y+60);
+            Actual.Name = $"Actual_{number}";
+            Actual.TabIndex = number;
+            if(tour.IsActual)
+            {
+                Actual.Text = "Актуален";
+            }
+            else
+            {
+                Actual.Text = "Неактуален";
+            }
+
+            CountTicket.AutoSize = true;
+            CountTicket.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            CountTicket.Location = new System.Drawing.Point(x+100, y+60);
+            CountTicket.Name = $"CountTicket_{number}";
+            CountTicket.TabIndex = number;
+            CountTicket.Text = $"Билетов: {tour.TicketCount}";
+        }
+    }
+}
